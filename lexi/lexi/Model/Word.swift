8c7f36087bf1c73e3d06152a7fc068a6fcf16209
Hung Nguyen <hd.nguyen101@gmail.com>
2023-07-16 17:04:57 -0700

    Initial commit

---
+++ lexi/lexi/Model/Word.swift
@@ -0,0 +1,25 @@
+//
+//  Word.swift
+//  lexi
+//
+//  Created by Hung Nguyen on 7/16/23.
+//
+
+import Foundation
+
+protocol WordProtocol {
+    var word: String { get }
+    var results: [WordDetail] { get }
+}
+
+struct Word: WordProtocol, Codable {
+    var word: String
+    var results: [WordDetail]
+}
+
+struct WordDetail: Codable {
+    var definition: String
+    var partOfSpeech: String?
+    var synonyms: [String]?
+    var exampleUsages: [String]?
+}
---
+++ lexi/lexi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lexi
+//
+//  Created by Hung Nguyen on 7/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
