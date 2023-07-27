7a1351e52b2edcb85dd7580bbf582efe6360ef9d
Hung Nguyen <hd.nguyen101@gmail.com>
2023-07-26 17:18:22 -0700

    Removed playground which i had committed by accident

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-
-var greeting = "Hello, playground"
-
-var names: [String]?
-names = ["hung", "john", "bird"]
-if let names = names {
-    print("\(names.joined(separator: ", "))")
-}
